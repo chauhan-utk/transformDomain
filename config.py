ac0675e2c79b14274ff683490aa8d676e0513ec9
UtkarshChauhan <utkarsh.chauhan@gmail.com>
2018-02-20 22:54:52 +0530

    baseline model

---
+++ config.py
@@ -0,0 +1,5 @@
+domainData = {
+    'amazon' : '/home/utkrsh/data/officeDataset/amazon/images/',
+    'dslr' : '/home/utkrsh/data/officeDataset/dslr/images/',
+    'webcam' : '/home/utkrsh/data/officeDataset/webcam/images/',
+}
---
+++ main.py
@@ -0,0 +1,31 @@
+from config import domainData
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import time
+import os
+import copy
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomSizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Scale(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = domainData['amazon'] # 'amazon', 'dslr', 'webcam'
+
+train_dataset = datasets.ImageFolder(data_dir, data_transforms)
